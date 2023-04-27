705844ddd2696761b554edf7b275e357c5d1d00d
Yehezkiel Reynaldi Abraham <yehezkielreynaldi01@gmail.com>
2023-04-27 15:51:35 +0700

    Tugas 8 JS
    Tugas ini saya kirimkan untuk Niomic

---
+++ tugas8.js
@@ -0,0 +1,13 @@
+function motorCBR150R() {
+    let hondaCBR = {
+        tahun: 2022,
+        jenis: "Sport",
+        cc: 150,
+        warna: "Black Matte"
+    }
+
+    hondaCBR.cc = 979
+    console.log(hondaCBR);
+}
+
+motorCBR150R();
